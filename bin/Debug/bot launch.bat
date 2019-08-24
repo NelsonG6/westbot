5ec5f9f6a8064ae101d4ab39798be5d05a3d3bb1
Nelson <18473267+NelsonG6@users.noreply.github.com>
2019-08-24 02:41:37 +0000

    update

---
+++ bin/Debug/bot launch.bat
@@ -0,0 +1,8 @@
+@echo off
+start cmd /k westbot.exe birdbot
+start cmd /k westbot.exe beebot
+start cmd /k westbot.exe tapbot
+start cmd /k westbot.exe westbot
+start cmd /k westbot.exe gemsbot
+@echo done
+
