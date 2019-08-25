ede09688ca1f6dac8389b110ebf69d01bae2e2f1
Nelson <18473267+NelsonRomaine@users.noreply.github.com>
2019-08-25 11:15:35 -0700

    executable files

---
+++ Executable/bot launch.bat
@@ -0,0 +1,8 @@
+@echo off
+start cmd /k experiment.exe birdbot
+start cmd /k experiment.exe beebot
+start cmd /k experiment.exe tapbot
+start cmd /k experiment.exe westbot
+start cmd /k experiment.exe gemsbot
+@echo done
+
